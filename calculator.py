1f84b98268ea95bf0ca4a45ccc1ca5034292ee0d
jyrikes <jyrikes3@gmail.com>
2024-04-16 17:58:42 -0300

    Commit inicial

---
+++ calculator.py
@@ -0,0 +1,26 @@
+class Calculator:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def soma():
+        return
+    
+    def subtracao():
+        return
+    
+    def multiplicacao():
+        return
+    
+    def divisao():
+        return
+    
+    def potencia():
+        return
+    
+    def raiz_quadrada():
+        return
+    
+    def fatorial():
+        return
+        
---
+++ tests/test_calculator.py
@@ -0,0 +1,10 @@
+import unittest
+from calculator import Calculator
+
+class Test_calculator(unittes.TestCase):
+    
+    
+    def test_soma(self):
+        self.assertEqual()
+        
+    
