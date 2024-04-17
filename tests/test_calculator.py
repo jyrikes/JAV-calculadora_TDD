9e965416977543885b5e81d475f0c73afa6f2758
ViniciusLira1311 <52613474+ViniciusLira1311@users.noreply.github.com>
2024-04-17 12:41:47 -0300

    Adicionando testes de soma

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,21 @@
 import unittest
 from calculator import Calculator
 
-class Test_calculator(unittes.TestCase):
+class Test_calculator(unittest.TestCase):
     
-    
+    @test
     def test_soma(self):
-        self.assertEqual()
+        c = Calculator(2, 8)
+        self.assertEqual(c.soma(), 10)
         
-    
+    def test_soma_2(self):
+        c = Calculator(-3, 7)
+        self.assertEqual(c.soma(), 4)
+
+    def teste_soma_3(self):
+        c = Calculator(-3, -4)
+        self.assertEqual(c.soma(), -7)
+
+    def teste_soma_4(self):
+        c =  Calculator(0, 10)
+        self.assertEqual(c.soma(), 10)
